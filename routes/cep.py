78850dff9fd89554d8de87bf99e1f1e45b626d60
mrcode-sys <marcosgabrriel91@gmail.com>
2026-01-27 09:48:41 -0400

    added directory services

--- routes/cep.py
+++ routes/cep.py
@@ -2,78 +2,17 @@ from flask import Blueprint, jsonify
 from models.cep import Cep
 import requests
 from extensions import db
+from services.cep_service import Search_cep
 
 cep_bp = Blueprint("cep", __name__, url_prefix="/api/cep")
 
 @cep_bp.route("<val>")
-def search_cep(val):
-    
-    infs = [       
-        "cep",
-        "localidade",
-        "uf",
-        "regiao",
-        "bairro",
-        "complemento",
-        "logradouro",
-        "ibge",
-        "gia",
-        "ddd",
-        "siafi"
-        ]
-    not_infs = []
-
-    val = val.replace("-", "")
-
-    cep_db = Cep.query.filter_by(cep=val).first()
-
-    if cep_db:
-        not_inf = False
-        for i in infs:
-            if getattr(cep_db, i, None) is None:
-                not_infs.append(i)
-                not_inf = True
-
-        print("Carregando do Banco de Dados")
-        if not_inf == False:
-            return jsonify(cep_db.to_dict()), 200
-        else:
-            print("Ausência de informação no database")
-
-    url = f"https://viacep.com.br/ws/{val}/json/"
-    resp = requests.get(url)
-
-    if resp.status_code != 200:
-        return jsonify({"erro":True}), 502
-
-    data = resp.json()
-    if "erro" in data:
+def request_cep(val):
+    try:
+        sc = Search_cep(val)
+        data = sc.search()
+        return jsonify(data), 200
+    except ValueError:
         return jsonify({"erro":True}), 400
-    
-    if not_infs and cep_db:
-        for i in not_infs:
-            setattr(cep_db, i, data.get(i))
-
-        db.session.commit()
-
-        return jsonify(cep_db.to_dict()), 200
-
-    print("Salvando CEP")
-    new = Cep(
-        cep = val,
-        localidade = data.get("localidade"),
-        uf = data.get("uf"),
-        regiao = data.get("regiao"),
-        bairro = data.get("bairro"),
-        complemento = data.get("complemento"),
-        logradouro = data.get("logradouro"),
-        ibge = data.get("ibge"),
-        gia = data.get("gia"),
-        ddd = data.get("ddd"),
-        siafi = data.get("siafi"),
-    )
-
-    db.session.add(new)
-    db.session.commit()
-
-    return jsonify(new.to_dict()), 200
+    except RuntimeError:
+        return jsonify({"erro":True}), 502
---
+++ services/cep_service.py
@@ -0,0 +1,97 @@
+from models.cep import Cep
+import requests
+from extensions import db
+
+class Search_cep():
+    def __init__(self, val):
+
+        self.val = val.replace("-", "")
+        self.cep_db = Cep.query.filter_by(cep=self.val).first()
+
+        self.infs = [       
+            "cep",
+            "localidade",
+            "uf",
+            "regiao",
+            "bairro",
+            "complemento",
+            "logradouro",
+            "ibge",
+            "gia",
+            "ddd",
+            "siafi"
+        ]
+        self.not_infs = []
+    def search(self):
+        data = self.database_search()
+
+        if not data:
+            data = self.api_search()
+
+        return data
+
+    def database_search(self):
+
+        if self.cep_db:
+            self.not_in_inf()
+
+            print("Carregando do Banco de Dados")
+            if not self.not_infs:
+                return self.cep_db.to_dict()
+
+            else:
+                print("Ausência de informação no database")
+
+    def api_search(self):
+        url = f"https://viacep.com.br/ws/{self.val}/json/"
+        resp = requests.get(url)
+        print(url)
+        print(resp.status_code)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Erro na API externa")
+
+        data = resp.json()
+        if "erro" in data:
+            raise ValueError("CEP inválido")
+        
+        if self.not_infs:
+            self.updt_db(data)
+        else:
+            self.save_cep(data)
+        return data
+
+    def save_cep(self, data):
+        print("Salvando CEP")
+        new = Cep(
+            cep = self.val,
+            localidade = data.get("localidade"),
+            uf = data.get("uf"),
+            regiao = data.get("regiao"),
+            bairro = data.get("bairro"),
+            complemento = data.get("complemento"),
+            logradouro = data.get("logradouro"),
+            ibge = data.get("ibge"),
+            gia = data.get("gia"),
+            ddd = data.get("ddd"),
+            siafi = data.get("siafi"),
+        )
+
+        db.session.add(new)
+        db.session.commit()
+
+        return new.to_dict()
+
+    def updt_db(self, data):
+        for i in self.not_infs:
+            setattr(self.cep_db, i, data.get(i))
+
+        db.session.commit()
+
+        return self.cep_db.to_dict()
+
+    def not_in_inf(self):
+        self.not_infs.clear()
+        for i in self.infs:
+            if getattr(self.cep_db, i, None) is None:
+                self.not_infs.append(i)
