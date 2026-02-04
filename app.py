bc261a9b28b203108989b9675b37b4e2698d957e
mrcode-sys <marcosgabrriel91@gmail.com>
2026-02-04 09:39:17 -0400

    Update on errors system

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify
 from extensions import db
 from flask_migrate import Migrate
 from routes.cep import cep_bp
-from errors.errors import ExternalAPIError, InvalidCEPError
+from errors.errors import AppError
 from utils.responses import error_response
 app = Flask(__name__)
 
@@ -19,18 +19,10 @@ app.register_blueprint(cep_bp)
 def index():
     return render_template("index.html")
 
-@app.errorhandler(InvalidCEPError)
-def handle_inv_cep_error(error):
-    return error_response("Invalid_CEP_Error", 400)
-
-@app.errorhandler(ExternalAPIError)
-def handle_ext_api_error(error):
-    return error_response("External_API_Error", 503)
-
-@app.errorhandler(Exception)
-def handle_generic_error(error):
+@app.errorhandler(AppError)
+def handle_app_error(error):
     app.logger.exception(error)
-    return error_response("Internal_Error", 500)
+    return error_response(error.code, error.status_code)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- errors/errors.py
+++ errors/errors.py
@@ -1,4 +1,11 @@
-class ExternalAPIError(Exception):
-    pass
-class InvalidCEPError(Exception):
-    pass
+class AppError(Exception):
+    code = "Internal_Error"
+    status_code = 500
+    
+class InvalidCEPError(AppError):
+    code = "Invalid_CEP_Error"
+    status_code = 400
+
+class ExternalAPIError(AppError):
+    code = "External_API_Error"
+    status_code = 503
