5ecdca6633e7bac8781d499ce84ceac16abc41a4
mrcode-sys <marcosgabrriel91@gmail.com>
2026-02-06 21:35:42 -0400

    added tests for time validation to cached CEPs

--- tests/test_cep.py
+++ tests/test_cep.py
@@ -1,3 +1,11 @@
+from datetime import datetime, timedelta
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from models.cep import Cep
+
 def test_valid_infs_on_cep_api(client):
     resp = client.get("/api/cep/01001000")
 
@@ -45,3 +53,40 @@ def test_cep_error_format(client):
 
     assert resp.status_code == 400
     assert data["success"] == False
+
+def test_cache_valid(client):
+
+    client.get("/api/cep/01001000")
+
+    from extensions import db
+    cep_db = Cep.query.filter_by(cep="01001000").first()
+
+    response = client.get("/api/cep/01001000")
+    data = response.get_json()
+
+    cep_db.updated_at = datetime.now()
+
+    db.session.commit()
+
+    assert response.status_code == 200
+    assert data["cached"] is True
+
+def test_cache_expired(client):
+    expired_time = datetime.now() - timedelta(hours=25)
+
+    cep = Cep(
+        cep="01001000",
+        localidade="São Paulo",
+        uf="SP",
+        updated_at=expired_time
+    )
+
+    from extensions import db
+    db.session.add(cep)
+    db.session.commit()
+
+    response = client.get("/api/cep/01001000")
+    data = response.get_json()
+
+    assert response.status_code == 201
+    assert data["cached"] is False
